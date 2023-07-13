5d52aa7691a6a0dbee489e1b92d4470ec5ca3a34
pbuchiumi <uchiumi@primebrains.co.jp>
2023-07-13 23:05:16 +0900

    hello-Vue12

--- app.js
+++ app.js
@@ -8,8 +8,15 @@ methods: {
     sayGoodbye() {
         this.message = 'Goodbye Vue!';
     },
-    changeMessage(message) {
-        this.message = message;
-    }
+    changeMessage(event,message) {
+        
+this.message = message;
+    console.log(event.type);
+    console.log(event.target);
+    // すべてのイベント･プロパティを参照
+    // for (let prop in event) {
+    //     console.log(prop + ': ' + event[prop]);
+    // }
+}
 }
 }).mount('#app');
