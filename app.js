ed12ea8c52ba4e355d7e98d73943b08a84a8b20d
pbuchiumi <uchiumi@primebrains.co.jp>
2023-07-13 23:02:00 +0900

    hello-Vue12

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 
 app = Vue.createApp({
     data() {
-        return { message: 'Hello Vue!' };
+        return { message: 'Hello Vue!'};
+    },
+    
+methods: {
+    sayGoodbye() {
+        this.message = 'Goodbye Vue!';
+    },
+    changeMessage(message) {
+        this.message = message;
     }
+}
 }).mount('#app');
