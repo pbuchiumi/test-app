5b102b6f70499f0ee475e08aecc984a0f0071fb9
pbuchiumi <uchiumi@primebrains.co.jp>
2023-07-13 22:35:13 +0900

    hello-Vue11

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 
 app = Vue.createApp({
     data() {
-        return {
-            colors: ['Red', 'Blue', 'Yellow'], // 単純な配列
-            users: [ // オブジェクト配列
-                { id: 1, name: 'John', age: 19 },
-                { id: 2, name: 'Eric', age: 32 },
-                { id: 3, name: 'Bill', age: 25 },
-            ]
-        };
+        return { message: 'Hello Vue!' };
     }
 }).mount('#app');
--- hello-vue/src/components/HelloWorld.vue
+++ hello-vue/src/components/HelloWorld.vue
@@ -3,7 +3,6 @@
     <p class="counter_label">{{ count_number }}</p>
     <a>count up</a>
     <a>reset</a>
-   
   </div>
 </template>
 
@@ -16,7 +15,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
