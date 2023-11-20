d564dcc9e29cd328604d8b327cccd7255b7f0da9
uchiumi <uchiumi@primebrains.co.jp>
2023-11-20 20:20:58 +0900

    commit1

--- hello-vue/src/components/HelloWorld.vue
+++ hello-vue/src/components/HelloWorld.vue
@@ -1,33 +1,42 @@
 <template>
   <div>
-    <p class="counter_label">{{ count_number }}</p>
-    <a>count up</a>
-    <a>reset</a>
+    <input v-model="userInput" type="number" />
+    <button @click="submitInput">決定</button>
+    <div v-if="allInputs.length === 4">
+      <h2>入力結果（降順）:</h2>
+      <ul>
+        <li v-for="input in sortedInputs" :key="input.userId">
+          User {{ input.userId }}: {{ input.value }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import axios from 'axios';
 
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+export default {
+  data() {
+    return {
+      userInput: '',
+      allInputs: []
+    };
+  },
+  computed: {
+    sortedInputs() {
+      return [...this.allInputs].sort((a, b) => b.value - a.value);
+    }
+  },
+  methods: {
+    async submitInput() {
+      const response = await axios.post('/api/submit', {
+        userId: this.$route.params.userId,
+        value: this.userInput
+      });
+      this.allInputs = response.data;
+      this.userInput = '';
+    }
+  }
+};
+</script>
--- hello-vue/src/main.js
+++ hello-vue/src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+const express = require('express');
+const app = express();
+app.use(express.json());
 
-createApp(App).mount('#app')
+let userInputs = [];
+
+app.post('/api/submit', (req, res) => {
+  userInputs.push(req.body);
+  if (userInputs.length === 4) {
+    userInputs.sort((a, b) => b.value - a.value);
+  }
+  res.json(userInputs);
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
